7abc7696c638ba0dbb065e71a65ed54586138123
Kellen <70085049+Kellenn@users.noreply.github.com>
2024-06-10 18:54:37 -0600

    Use unixepoch instead of strftime

--- setup.sql
+++ setup.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TABLE IF NOT EXISTS logins (
     login_id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INT,
-    login_time TIMESTAMP DEFAULT (strftime('%s', 'now')),
+    login_time TIMESTAMP DEFAULT (unixepoch()),
     logout_time TIMESTAMP NULL,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
