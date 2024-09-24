ed6243dc652074f973157c58b03859f8386cbba0
Kellen <70085049+Kellenn@users.noreply.github.com>
2024-09-23 20:25:14 -0600

    Te

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ FORCE_LOGOUT_HOUR = 22 # At what hour should we force logout all users (24-hour)
 
 SUBNETS = ["192.168.4.*", "192.168.5.*", "192.168.6.*", "192.168.7.*"] # A list of networks in a notation recognized by Nmap to scan for active devices.
 
-SCAN_INTERVAL = 60 # How often to scan the network for users.
+SCAN_INTERVAL = 15 # How often to scan the network for users.
 
 DEBOUNCE_SECONDS = SCAN_INTERVAL * 30 # Time threshold to debounce network absence before logging out users.
