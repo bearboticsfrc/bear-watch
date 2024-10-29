e9527251a152ae8e5624a709de80d119d66983c6
Kellen <70085049+Kellenn@users.noreply.github.com>
2024-10-28 19:58:01 -0600

    Remove old logic

--- app/watcher.py
+++ app/watcher.py
@@ -8,7 +8,6 @@ from typing import TYPE_CHECKING, Literal
 
 from app.models import NetworkUser
 from app.tracker import Tracker
-from app.utils import compute_logout_timedelta
 
 if TYPE_CHECKING:
     import asqlite
@@ -48,11 +47,6 @@ class Watcher:
                 await connection.executescript(fp.read())
 
         await self._populate_users()
-
-        _log.debug("Starting force logout task.")
-        self.logout_task = asyncio.create_task(
-            self._logout_task(), name="Watcher force-logout task"
-        )
 
         _log.debug("Starting tracker task.")
         self.tracker_task = asyncio.create_task(
@@ -99,7 +93,7 @@ class Watcher:
 
     async def cleanup(self) -> None:
         """Cancels ongoing tasks and performs cleanup."""
-        tasks = [self.logout_task, self.tracker_task]
+        tasks = [self.tracker_task]
 
         for task in tasks:
             if task and not task.done():
@@ -127,20 +121,6 @@ class Watcher:
 
         if inactive_users:
             _log.info("Purged %d inactive users.", len(inactive_users))
-
-    async def _logout_task(self) -> None:
-        """
-        Periodically logs out users at FORCE_LOGOUT_HOUR.
-
-        This runs in an infinite loop, sleeping for a specified duration before
-        attempting to log out users still logged in.
-        """
-        while True:
-            sleep_seconds = compute_logout_timedelta()
-            _log.debug("Sleeping for %ds.", sleep_seconds)
-
-            await asyncio.sleep(sleep_seconds)
-            await self.logout("*")
 
     def get_user(self, mac: str | Literal["*"]) -> NetworkUser | None:
         """
